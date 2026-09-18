9c0f14789d2a44cb40c70bcddb3d3b7e4aa5df05
chloefriedman <271friedmanc@student.dist113.org>
2026-09-18 11:06:12 -0500

    updated square methods

--- src/MyAI.java
+++ src/MyAI.java
@@ -49,4 +49,80 @@ public class MyAI extends CellAI {
     {
         
     }
+    public static Location oneOffSquare(Grid grid)
+    {
+        int myID = getID();
+        int start = (int)(Math.random()*2);
+        if(start == 0)
+        {
+            for(int i = 0; i < grid.length(); i++)
+            {
+                for(int j = 0; j < grid[0].length(); j++)
+                {
+                    if(grid[i][j].getNeighbors() == 1 && grid[i][j] == myID)
+                    {
+                        return new Location(i,j);
+                    }
+                }
+            }
+        }
+        else
+        {
+            for(int i = grid.length-1; i >=0; i--)
+            {
+                for(int j = grid[0].length; j >= 0; j--)
+                {
+                    if(grid[i][j].getNeighbors() == 1 && grid[i][j] == myID)
+                    {
+                        return new Location(i,j);
+                    }
+            }
+        }
+        }
+            return new Location(-1,-1);
+
+    }
+
+
+
+
+    public static Location finishSquare(Grid grid)
+    {
+        if(oneOffSquare().getRow() == -1)
+        {
+            return new Location(-1,-1);
+        }
+        int myID = getID();
+        int start = (int)(Math.random()*2);
+        
+        for(int i = 0; i < grid.length(); i++)
+        {
+            for(int j = 0; j < grid[0].length(); j++)
+            {
+                if(grid[i][j].getNeighbors() == 1 && grid[i][j] == myID)
+                {
+                     for (int r = i - 1; r <= i + 1; r++) {
+                         for (int c = j - 1; c <= j + 1; c++) 
+                        {
+                             if (r == i && c == j)
+                             {
+                                 continue;
+                             }
+
+                            if (r >= 0 && c >= 0 && r < grid.getRows() && c < grid.getCols())
+                            {
+                                if (grid.getCell(r, c) != -1) 
+                                {
+                                    
+                                    if(r == i)
+                                    
+                                }
+                            }
+                            else
+                        }
+        }
+                }
+            }
+        }
+    }
 }
