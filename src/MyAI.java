d33ff0ca78b79cd7c0e3598742052d57cbf76324
chloefriedman <271friedmanc@student.dist113.org>
2026-09-21 14:45:37 -0500

    added to defense and finished square mwethod also made beehive

--- src/MyAI.java
+++ src/MyAI.java
@@ -2,7 +2,7 @@
  * STUDENT FILE
  *
  * Name: Chloe Friedman
- * AI Code Name: ______________________
+ * AI Code Name: unicornAI
  *
  * Strategy Description:
  * Replace this comment with a short explanation of the strategy your AI uses.
@@ -12,7 +12,7 @@ public class MyAI extends CellAI {
 
     @Override
     public String getAIName() {
-        return "MyAI - CHANGE ME";
+        return "unicornAI";
     }
 
     @Override
@@ -40,89 +40,259 @@ public class MyAI extends CellAI {
         return new Location(row,col);
     }
 
-    public static Location offense(Grid grid)
+    public Location attack(Grid grid)
     {
 
     }
 
-    public static Location defense(Grid grid)
+    public Location defend(Grid grid)
     {
-        
+        int numKills = 0;
+        int[][] current = new int[grid.getRows()][grid.getCols()];
+        int[][] next = new int[grid.getRows()][grid.getCols()];
+        int[][] compare = new int[grid.getRows()][grid.getCols()];
+        for(int i = 0; i < grid.getRows(); i++)
+        {
+            for(int j = 0; j < grid.getCols(); j++)
+            {
+                current[i][j] = grid.getCell(i,j);
+                next[i][j] = grid.getCell(i,j);
+            }
+        }
+        for(int i = 0; i < current.length; i++)
+        {
+            for(int j = 0; j < current[0].length; j++)
+            {
+                if(current[i][j] != -1 && current[i][j] != getID())
+                {
+                    int mostCommon = GridFunctions.mostCommonNeighbor(i,j,grid);
+                    if(mostCommon != -1 && mostCommon == current[i][j])
+                    {
+                        next[i][j] = -1;
+                        nextGen(next, grid, new Location(i,j));
+                    }
+                    else
+                    {
+                        next[i][j] = current[i][j];
+                    }
+                }
+                else
+                {
+                    next[i][j] = -1;
+                }
+            }
+        }
+
     }
-    public static Location oneOffSquare(Grid grid)
+    public Location oneOffSquare(Grid grid)
     {
         int myID = getID();
         int start = (int)(Math.random()*2);
         if(start == 0)
         {
-            for(int i = 0; i < grid.length(); i++)
+            for(int i = 0; i < grid.getRows(); i++)
             {
-                for(int j = 0; j < grid[0].length(); j++)
+                for(int j = 0; j < grid.getCols(); j++)
                 {
-                    if(grid[i][j].getNeighbors() == 1 && grid[i][j] == myID)
+                    if(GridFunctions.getNeighbors(i, j,grid) == 1 && grid.getCell(i,j) == myID)
                     {
-                        return new Location(i,j);
+                        for (int r = i - 1; r <= i + 1; r++) 
+                        {
+                            for (int c = j - 1; c <= j + 1; c++) 
+                            {
+                                if (r == i && c == j)
+                                {
+                                    continue;
+                                }
+
+                                if (r >= 0 && c >= 0 && r < grid.getRows() && c < grid.getCols())
+                                {
+                                    if (grid.getCell(r, c) != -1) 
+                                    {
+                                        if(r == i)
+                                        {
+                                            if(r != grid.getRows())
+                                            {
+                                                return new Location(r-1,c);
+                                            }
+                                            else
+                                            {
+                                                return new Location(r+1,c);
+                                            }
+                                        }
+                                        else if(c == j)
+                                        {
+                                            if(c != grid.getCols())
+                                            {
+                                                return new Location(r,c+1);
+                                            }
+                                            else
+                                            {
+                                                return new Location(r, c-1);
+                                            }
+                                        }
+                                        else if(c > j)
+                                        {
+                                            return new Location(r,c+1);
+                                        }
+                                        else
+                                        {
+                                            return new Location(r,c-1);
+                                        }
+                                    
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
         else
         {
-            for(int i = grid.length-1; i >=0; i--)
+            for(int i = grid.getRows()-1; i >=0; i--)
             {
-                for(int j = grid[0].length; j >= 0; j--)
+                for(int j = grid.getCols()-1; j >= 0; j--)
                 {
-                    if(grid[i][j].getNeighbors() == 1 && grid[i][j] == myID)
+                    if(GridFunctions.getNeighbors(i, j,grid) == 1 && grid.getCell(i,j) == myID)
                     {
-                        return new Location(i,j);
-                    }
-            }
-        }
-        }
-            return new Location(-1,-1);
-
-    }
-
-
-
-
-    public static Location finishSquare(Grid grid)
-    {
-        if(oneOffSquare().getRow() == -1)
-        {
-            return new Location(-1,-1);
-        }
-        int myID = getID();
-        int start = (int)(Math.random()*2);
-        
-        for(int i = 0; i < grid.length(); i++)
-        {
-            for(int j = 0; j < grid[0].length(); j++)
-            {
-                if(grid[i][j].getNeighbors() == 1 && grid[i][j] == myID)
-                {
-                     for (int r = i - 1; r <= i + 1; r++) {
-                         for (int c = j - 1; c <= j + 1; c++) 
+                        for (int r = i - 1; r <= i + 1; r++) 
                         {
-                             if (r == i && c == j)
-                             {
-                                 continue;
-                             }
-
-                            if (r >= 0 && c >= 0 && r < grid.getRows() && c < grid.getCols())
+                            for (int c = j - 1; c <= j + 1; c++) 
                             {
-                                if (grid.getCell(r, c) != -1) 
+                                if (r == i && c == j)
                                 {
+                                    continue;
+                                }
+
+                                if (r >= 0 && c >= 0 && r < grid.getRows() && c < grid.getCols())
+                                {
+                                    if (grid.getCell(r, c) != -1) 
+                                    {
+                                        if(r == i)
+                                        {
+                                            if(r != grid.getRows())
+                                            {
+                                                return new Location(r-1,c);
+                                            }
+                                            else
+                                            {
+                                                return new Location(r+1,c);
+                                            }
+                                        }
+                                        else if(c == j)
+                                        {
+                                            if(c != grid.getCols())
+                                            {
+                                                return new Location(r,c+1);
+                                            }
+                                            else
+                                            {
+                                                return new Location(r, c-1);
+                                            }
+                                        }
+                                        else if(c > j)
+                                        {
+                                            return new Location(r,c+1);
+                                        }
+                                        else
+                                        {
+                                            return new Location(r,c-1);
+                                        }
                                     
-                                    if(r == i)
-                                    
+                                    }
                                 }
                             }
-                            else
                         }
-        }
+                    }
                 }
             }
         }
+            return new Location(-1,-1);
+
     }
+
+    public Location beeHive(Grid grid)
+    {
+        int myID = getID();
+        for(int i = 0; i < grid.getRows()-2; i++)
+        {
+            for(int j = 0; j < grid.getCols()-3; j++)
+            {
+                int count = 0;
+                Location missing = new Location(-1,-1);
+                if(grid.getCell(i,j) == -1)
+                {
+                    if(grid.getCell(i,j+1) == myID)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        missing = new Location(i,j+1);
+                    }
+                    if(grid.getCell(i,j+2) == myID)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        missing = new Location(i,j-1);
+                    }
+                    if(grid.getCell(i+1,j) == myID)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        missing = new Location(i+1,j);
+                    }
+                    if(grid.getCell(i+1,j+3) == myID)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        missing = new Location(i+1,j+3);
+                    }
+                    if(grid.getCell(i+2,j+1) == myID)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        missing = new Location(i+2,j+1);
+                    }
+                    if(grid.getCell(i+2,j+2) == myID)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        missing = new Location(i+2,j+2);
+                    }
+                    if(count == 5)
+                    {
+                        return missing;
+                    }
+                }
+            }
+        }
+        return new Location(-1, -1);
+    }
+
+    public void nextGen(int[][] next, Grid grid, Location loc)
+    {
+        Grid updatedGrid = new Grid(next);
+
+        
+        
+    }
+    
+
+
+
+
+    
 }
+
